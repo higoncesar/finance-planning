a2d38d886a6aeadd502ea55ef9394a1716911d21
Higon Cesar <higon_cs@hotmail.com>
2025-04-07 03:23:44 -0300

    chore: config vitest coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vitest/config';
 import path from 'path';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
     setupFiles: [],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.spec.ts', 'src/**/*.test.ts', 'src/**/__tests__/**', '**/*.d.ts'],
+    },
   },
   resolve: {
     alias: {
